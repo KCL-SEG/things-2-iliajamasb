835eeb0d9e451835b8ef1fbd4ffb7844df3c3816
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-10-14 19:16:06 +0100

    Nearly working things v2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,11 @@
 """Forms of the project."""
+from django import forms
+
+class registerThing(forms.Form):
+
+    name = forms.CharField(label='Name')
+    description = forms.CharField(label='Description')
+    quantity = forms.IntegerField(label='Quantity')
+    widgets = { "bio": forms.Textarea() }
 
 # Create your forms here.
--- things/views.py
+++ things/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import registerThing
 
 def home(request):
+    form = registerThing()
     return render(request, 'home.html', {'form': form})

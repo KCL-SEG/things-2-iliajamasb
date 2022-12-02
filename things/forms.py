769e41a1a32210f14d580da36049d74a234fbb50
iliajamasb <99290474+iliajamasb@users.noreply.github.com>
2022-12-02 01:27:38 +0000

    bug hunt

--- things/forms.py
+++ things/forms.py
@@ -3,9 +3,12 @@ from django import forms
 
 class ThingForm(forms.Form):
 
-    name = forms.CharField(label='Name')
-    description = forms.CharField(label='Description')
-    quantity = forms.IntegerField(label='Quantity')
-    widgets = { "bio": forms.Textarea() }
+    name = forms.CharField(label='Name', max_length=35, blank=False, unique=True)
+    description = forms.CharField(label='Description', blank=False)
+    quantity = forms.IntegerField(
+        label='Quantity', validators=[
+            MinValueValidator(0),
+            MaxValueValidator(50)])
+    widgets = { "description": forms.Textarea(), "quantity": forms.NumberInput() }
 
 # Create your forms here.

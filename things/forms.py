3032c9583256c983d124786a4b11af80a7b57aa6
iliajamasb <99290474+iliajamasb@users.noreply.github.com>
2022-12-02 01:52:11 +0000

    dumbest task

--- things/forms.py
+++ things/forms.py
@@ -1,16 +1,18 @@
 """Forms of the project."""
 from django import forms
-from django.core.validators import MaxValueValidator, MinValueValidator, MinLengthValidator
-
+from things import models
 
 class ThingForm(forms.Form):
 
-    name = forms.CharField(label='Name')
-    #max_length=35,
-    #blank=False,
-    #unique=True)
+    class Meta:
+
+        model = models.Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = { "description": forms.Textarea(), "quantity": forms.NumberInput() }
+
+    name = forms.CharField(label='Name', validators=[])
     description = forms.CharField(label='Description')
-    quantity = forms.IntegerField(label='Quantity', validators=[MinValueValidator(0),MaxValueValidator(50)])
-    widgets = { "description": forms.Textarea(), "quantity": forms.NumberInput() }
+    quantity = forms.IntegerField(label='Quantity')
+
 
 # Create your forms here.

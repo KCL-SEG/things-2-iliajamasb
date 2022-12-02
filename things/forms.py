0aaf9260ec326e312f7af2cce37719af8f4f1fa5
iliajamasb <99290474+iliajamasb@users.noreply.github.com>
2022-12-02 02:03:02 +0000

    Nearly done

--- things/forms.py
+++ things/forms.py
@@ -5,17 +5,18 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class ThingForm(forms.Form):
 
-    class Meta:
+    #class Meta:
 
-        model = models.Thing
-        fields = ['name', 'description', 'quantity']
-        widgets = { "description": forms.Textarea(), "quantity": forms.NumberInput() }
+        #model = models.Thing
+        #fields = ['name', 'description', 'quantity']
+
 
     name = forms.CharField(label='Name', max_length=35, required=True)
-    description = forms.CharField(label='Description', max_length=120, required=False)
+    description = forms.CharField(label='Description', max_length=120, required=True)
     quantity = forms.IntegerField(
         validators=[MinValueValidator(0),MaxValueValidator(50)]
     )
+    widgets = { "description": forms.Textarea(), "quantity": forms.NumberInput() }
 
 
 # Create your forms here.
